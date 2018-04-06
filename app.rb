0d0901e426fc8e7938757360b226aa0066c717e9
David Elster <davidandtheweb@gmail.com>
2018-04-05 22:03:01 -0400

    home, user, users, default layouts

--- app.rb
+++ app.rb
@@ -5,53 +5,57 @@ require 'sinatra/flash'
 require './models'
 enable :sessions
 
-set :database, "sqlite3:microblog.sqlite3"
+set :database, 'sqlite3:microblog.sqlite3'
 
 def current_user
-    if session[:user_id]
-        @current_user = User.find(session[:user_id])
-    end
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
 end
 
 get '/' do
-    erb :home
+  erb :home
 end
 
-get '/about' do
-    erb :about
+get '/signup' do
+  erb :signup
 end
 
-get '/users' do
-    @users = User.all
-    erb :users
+get '/login' do
+  erb :login
 end
 
 get '/user/:id' do
-    @user = User.find(params[:id])
-    @posts = @user.posts
-    erb :user
+  @user = User.find(params[:id])
+  @posts = @user.posts
+  erb :user
+end
+
+get '/users' do
+  @users = User.all
+  erb :users
 end
 
 post '/users/new' do
     puts "****************************"
     puts params
     puts "****************************"
-    User.create(params[:post])
-    redirect '/users'
+  User.create(params[:new_user])
+  redirect "/user/#{User.last.id}"
 end
 
 post '/sign-in' do
-    @user = User.where(fname: params[:fname]).first
-    if @user.password == params[:password]
-        session[:user_id] = @user.id
-        flash[:notice] = "Success!"
-        redirect '/'
-    else
-        flash[:notice] = "FAILED LOGIN :("
-        redirect '/sign-in-failed'
-    end
+  @user = User.where(user_name: params[:user_name]).first
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    flash[:notice] = 'Success!'
+    redirect "/user/#{@user.user_name}"
+  else
+    flash[:notice] = 'FAILED LOGIN :('
+    redirect '/sign-in-failed'
+  end
 end
 
 get '/sign-in-failed' do
-    erb :sign_in_failed
+  erb :sign_in_failed
 end
